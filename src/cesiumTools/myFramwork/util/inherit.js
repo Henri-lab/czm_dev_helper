2bc14088b5be26db1936faaa732fcc4462c91377
fox <1842208351@qq.com>
2024-06-24 23:07:05 +0800

    新增模块

---
+++ src/cesiumTools/myFramwork/util/inherit.js
@@ -0,0 +1,17 @@
+/**
+   * 函数继承
+   * @param {Function} childCtor - 子函数
+   * @param {Function} parentCtor - 父函数
+   * @returns {Object} 返回继承后的原型对象
+   */
+export default function inherits(childCtor, parentCtor) {
+    function TempCtor() { }
+    TempCtor.prototype = parentCtor.prototype;
+    childCtor.superClass_ = parentCtor.prototype;
+    childCtor.prototype = new TempCtor();
+    childCtor.prototype.constructor = childCtor;
+    childCtor.base = function (me, methodName, varArgs) {
+        const args = Array.prototype.slice.call(arguments, 2);
+        return parentCtor.prototype[methodName].apply(me, args);
+    };
+}
