a8081c5237413dcf951bdb99a8f9167e6ef698b2
fox <1842208351@qq.com>
2024-06-25 18:07:16 +0800

    全局常量

---
+++ src/cesium_dev_helper/czmHelper/util/CONST.js
@@ -0,0 +1,33 @@
+const providerTypes = [
+    'ArcGisMapServerImageryProvider',
+    'ArcGISTiledElevationTerrainProvider',
+    'BingMapsImageryProvider',
+    'Cesium3DTilesVoxelProvider',
+    'CesiumTerrainProvider',
+    'CustomHeightmapTerrainProvider',
+    'EllipsoidTerrainProvider',
+    'GoogleEarthEnterpriseImageryProvider',
+    'GoogleEarthEnterpriseMapsProvider',
+    'GoogleEarthEnterpriseTerrainProvider',
+    'GridImageryProvider',
+    'I3SDataProvider',
+    'ImageryProvider',
+    'IonImageryProvider',
+    'MapboxImageryProvider',
+    'MapboxStyleImageryProvider',
+    'OpenStreetMapImageryProvider',
+    'SingleTileImageryProvider',
+    'TerrainProvider',
+    'TileCoordinatesImageryProvider',
+    'TileMapServiceImageryProvider',
+    'TileProviderError',
+    'UrlTemplateImageryProvider',
+    'VoxelProvider',
+    'VRTheWorldTerrainProvider',
+    'WebMapServiceImageryProvider',
+    'WebMapTileServiceImageryProvider'
+]
+
+export {
+    providerTypes
+}
