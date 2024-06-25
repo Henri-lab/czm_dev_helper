183bc2ad51e3f4bbbd27d7c04135c50d1a643bc4
fox <1842208351@qq.com>
2024-06-25 16:13:24 +0800

    ''

--- src/cesium_dev_helper/traffic/metro/index.js
+++ src/cesium_dev_helper/traffic/metro/index.js
@@ -1,13 +1,13 @@
 import { renderLines, removeAllLines, hideLineByName } from "./line";
 import { renderStation, removeAllStations, hideStationByName } from "./stations";
 import { renderStationBill, removeAllBillboards } from "./billboards";
-import _PopupLoader /*类名和实例名重复的处理*/ from "../../myFramwork/Bubble/PopupLoader";
+import _PopupLoader /*类名和实例名重复的处理*/ from "../../czmHelper/Bubble/PopupLoader";
 import { findStationByName } from "./stations";
 import {
     MaterialCreator,
     GeometryCreater,
     EffectController
-} from '../../myFramwork/Effect'
+} from '../../czmHelper/Effect'
 import { changeDisplayBillBoard } from './billboards';
 
 
