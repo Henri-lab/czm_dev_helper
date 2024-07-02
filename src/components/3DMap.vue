5884957fa13c0187c039ac035694e617d07a5b26
fox <1842208351@qq.com>
2024-07-02 20:52:17 +0800

    改为在vue中加载地图

---
+++ src/components/3DMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="czm-container">
+    <div id="czm-viewer"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import initViewer from '../util/initViewer';
+
+onMounted(() => {
+  const el = { id: 'czm-container' };
+  initViewer(el);
+});
+</script>
+
+<style lang="scss" scoped>
+#czm-container {
+  width: 100%;
+  height: 700px;
+  background-color: bisque;
+  position: relative;
+  #czm-viewer {
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+  }
+}
+</style>
