b42c038d44565e2ddf2d25a2278216a10df509fe
fox <1842208351@qq.com>
2024-08-16 11:01:59 +0800

    ‘’

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <Editor />
   <!-- <v-scale-screen width="1920" height="1080" delay="200">
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
   </v-scale-screen> -->
+
+  <!-- test -->
+  <!-- <Editor /> -->
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
-import Editor from './components/Editor/index.vue'
 
-onMounted(() => {
-  console.log('app mounted')
-});
+<script setup>
+// test
+// import Editor from './components/Editor/index.vue'
+
 </script>
 <style lang="scss"></style>
