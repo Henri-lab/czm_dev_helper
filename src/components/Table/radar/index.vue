4571eec80078e7a8979ec6887d8e1a6326eabd87
fox <1842208351@qq.com>
2024-08-23 13:10:38 +0800

    ’‘

--- src/components/Table/radar/index.vue
+++ src/components/Table/radar/index.vue
@@ -28,10 +28,9 @@
                     </el-input>
                     <!--字段名称 雷达型号 ； 字段类型 下拉框+输入框  @hf-->
                     <el-select v-if="opt.name === '雷达型号'" style="width: 180px" v-model="selRadars[table_i]"
-                        :placeholder=selRadars[table_i] @change="setSelRadarType(table, table_i, $event)" allow-create
+                        :placeholder=selRadars[table_i] @change="completeRadarParams(table, table_i, $event)" allow-create
                         filterable>
-                        <el-option v-for="type_ in radarTypes" :key="type_" :label="type_" :value="type_"
-                            @click="getCurRadarParamsByType(table, table_i)" />
+                        <el-option v-for="type_ in radarTypes" :key="type_" :label="type_" :value="type_" />
                     </el-select>
                     <!-- 字段类型 自动补全 输入框 @hf-->
                     <el-input v-if="opt.type == 'text-auto'" v-model="opt.value" placeholder="">
@@ -47,7 +46,7 @@
 </template>
 <script setup>
 import { _template_, wrapObj, tableConfig as tbInit } from './config';
-import { getRadar } from '@/api/';
+import { getRadar } from '@/api/facility';
 
 const tableList = ref([]);
 // 添加设施或雷达 - 空白表单-带渲染方式 -
@@ -56,42 +55,50 @@ const addTable = () => {
     // console.log(tableList.value, `table*${tableList.value.length}`)
 };
 
-
 // < radar参数生成 @hf
-const radarTypes = ref([]);;//数据库已有radar的类型(数据库缓存)
-const db_Radars = ref([]);//数据库已有radar的全部信息(数据库缓存)
-let manuRadar = _template_;//初始化  赋值表单渲染源 
-const selRadars = ref([]);//每个表单的雷达类型选择
+const db_RadarTypes = ref([]);
+const db_Radars = ref([]);//数据库已有radar(数据库缓存)
+const selRadars = [];
 const getAllRadarInfos = async () => {
     console.log('get all radar infos ')
     const res = await getRadar()
     if (res.code != 200) return
     const radarInfos = res.data
-    radarTypes.value = radarInfos.map(item => item.radar)
+    db_RadarTypes.value = radarInfos.map(item => item.radar)
     db_Radars.value = radarInfos.map(item => item)
 }
 onMounted(() => {
     getAllRadarInfos()
 })
 // 点击option设置雷达类型
-function setSelRadarType(table, indexOfTable, selValue) {//传入的值一样的 但是index不同
-    selRadars.value[indexOfTable] = selValue
-    table._meta_.selRadar = selRadars.value[indexOfTable]
+function completeRadarParams(table, indexOfTable, selValue) {//传入的值一样的 但是index不同
+    table._meta_.selRadar = selValue
     console.log(`set radar type <${table._meta_.selRadar}> 在第${indexOfTable + 1}张表`)
-    manuRadar['radar'] = selValue
-    table.data = wrapObj(manuRadar)
-    console.log(table.data, 'table.data')
-}
+    const res = db_Radars.value.find(item => item.radar == selValue)
+    // 查询到了
+    if (res) {
+        console.log('查询到了')
+        console.log(`search <${selValue}> in db :`, res)
+        const orderedRes = {}
+        Object.keys(_init_).forEach(key => {
+            orderedRes[key] = res[key]
+        })
+        selRadars[indexOfTable] = selValue
+        // selRadars.value[indexOfTable] = {
+        //   type: selValue,
+        //   info: orderedRes
+        // }
+        table.data = JSON.parse(JSON.stringify(wrapObj(orderedRes)));
+    } else {
+        console.log('没有查询到')
+    }
 
-// 根据 雷达类别 返回 雷达参数 
-const getCurRadarParamsByType = async (table, indexOfTable) => {
-    const type = selRadars.value[indexOfTable]
-    // update every tableMeta
-    const res = db_Radars.value.find(item => item.radar == type)
-    console.log(`search <${type}> in db :`, res)
-    console.log(selRadars.value, 'selRadars')
-    console.log(tableList.value.map(item => item._meta_.selRadar), '每张表的radar选项')
-};
+    console.log(selRadars, '每张表应该的radar选项!!')
+    console.log('每张表实际的radar选项:')
+    tableList.value.forEach(item => console.log(item._meta_.selRadar, ','))
+    console.log(tableList.value, '目前 所有表')
+}
+// @hf>
 
 
 </script>
