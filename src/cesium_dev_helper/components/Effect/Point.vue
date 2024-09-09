6b9caadf73d9f1df9affea11f43d042b691ae019
fox <1842208351@qq.com>
2024-09-09 16:26:11 +0800

    point

--- src/cesium_dev_helper/components/Effect/Point.vue
+++ src/cesium_dev_helper/components/Effect/Point.vue
@@ -18,27 +18,58 @@ $bus.on('czmEntityEvent@henrifox', ({ viewer, editor }) => {
 })
 
 const props = defineProps({
-    colors: {
-        type: Object,
-        default: null
+    size: {
+        type: Number,
+        default: 100
     },
     position: {
         type: Object,
         default: () => Cesium.Cartesian3.fromDegrees(2.294481, 48.858370, 100)//eiffel tower
-    }
+    },
+    color: {
+        type: String,
+        default: null
+    },
+    colors: {
+        type: Object,
+        default: null
+    },
+    extraOpt: {
+        type: Object,
+        default: () => ({
+            outlineColor: Cesium.Color.WHITE,
+            outlineWidth: 3,
+            scaleByDistance: new Cesium.NearFarScalar(1000, 1.0, 5000, 0.0),
+            translucencyByDistance: new Cesium.NearFarScalar(1000, 1.0, 5000, 0.0),
+            disableDepthTestDistance: 1000 // 在1000米内禁用深度测试
+        })
+    },
+    zoom: {
+        type: Boolean,
+        default: true
+    },
+    three: {
+        type: Boolean,
+        default: false
+    },
+
+
 })
 
 let curSec = 0;
 let timer1, timer2
+let curEntity
+const createDynamicPoint = (_viewer_) => {
+    const colorsProp = props.colors
+    const positionProp = props.position
+    if (!_viewer_) return;
 
-const createDynamicPoint = (_viewer_, colorsProp, positionProp) => {
-    if (!_viewer_ || !colorsProp) return
-    // console.log(_viewer_)
     function parsedColor(string) {
         return Cesium.Color.fromCssColorString(string)
     }
     // 定义动态颜色变化函数
     let colorDefinition = function (time) {
+        if (!colorsProp) return;
         const colorsData = colorsProp.data;
         const endTime = colorsData[colorsData.length - 1].time + colorsProp.interval
         for (let i = 0; i < colorsData.length; i++) {
@@ -56,21 +87,21 @@ const createDynamicPoint = (_viewer_, colorsProp, positionProp) => {
         }
 
     }
-
     // 创建一个动态颜色属性
-    const dynamicColorProperty = new DynamicColorProperty(colorDefinition);
-
+    let dynamicColorProperty;
+    colorDefinition && (dynamicColorProperty = new DynamicColorProperty(colorDefinition));
     // 添加一个带有动态颜色的实体
+    curEntity && _viewer_.entities.remove(curEntity)
     const entity = _viewer_.entities.add({
         position: positionProp,
         point: {
-            pixelSize: 100,
-            color: dynamicColorProperty, // 使用动态颜色属性
+            pixelSize: props.size,
+            color: !props.color ? dynamicColorProperty : parsedColor(props.color), // 没有指定颜色 则使用动态颜色属性 ;动态属性不存在 则cesium使用默认白色
+            ...props.extraOpt,
         },
-
     });
-    _viewer_.zoomTo(entity);
-
+    curEntity = entity
+    props.zoom && _viewer_.zoomTo(entity);
     // 确保 Cesium 渲染循环正确地更新颜色
     _viewer_.scene.preRender.addEventListener(function (scene, time) {
         // 强制更新属性值
@@ -78,29 +109,18 @@ const createDynamicPoint = (_viewer_, colorsProp, positionProp) => {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
 onMounted(() => {
     timer1 = setInterval(() => {
         curSec++
     }, 1000)
-    timer2 = setTimeout(() => createDynamicPoint(_viewer_, props.colors, props.position), 10)
+    timer2 = setTimeout(() => createDynamicPoint(_viewer_), 10)
 })
-watch(() => props.colors, (newV) => {
-    createDynamicPoint(_viewer_, newV, props.position)
-})
-watch(() => props.position, (newV) => {
-    createDynamicPoint(_viewer_, props.colors, newV)
-})
+watch(() => props, (newV) => {
+    console.log(props.size)
+    curEntity && _viewer_.entities.remove(curEntity)
+    createDynamicPoint(_viewer_)
+}, { deep: true })
+
 
 
 onBeforeUnmount(() => {
--- src/cesium_dev_helper/example/ExPoint/index.vue
+++ src/cesium_dev_helper/example/ExPoint/index.vue
@@ -4,8 +4,10 @@
             <div class="code" ref="code"
                 style="color:antiquewhite; background-color: rgb(25, 27, 22); width: 50%; height: 20%; overflow: scroll; font-size: 16px;">
             </div>
+            <el-button @click="handleSize('+')">增加半径</el-button>
+            <el-button @click="handleSize('-')">减小半径</el-button>
             <CzmMap width="800px" height="1000px">
-                <Point :colors="colors" :position="position"></Point>
+                <Point :size="size" :color="fixedColor" :colors="colors" :position="position" :extraOpt="extraOpt"></Point>
             </CzmMap>
         </div>
 
@@ -20,9 +22,20 @@ import { onMounted, ref } from 'vue'
 import * as Cesium from 'cesium';
 import axios from 'axios'
 
-
+const size = ref(100)
+const handleSize = (type) => {
+    size.value += type == '+' ? 10 : -10
+}
+const fixedColor = ref()//如果指定一个固定颜色 则动态颜色失效
 const colors = ref()
 const position = ref(new Cesium.Cartesian3.fromDegrees(2.294481, 48.858370, 100))
+const extraOpt = ref({
+    outlineColor: Cesium.Color.WHITE,
+    outlineWidth: 3,
+    scaleByDistance: new Cesium.NearFarScalar(1000, 1.0, 5000, 0.0),
+    translucencyByDistance: new Cesium.NearFarScalar(1000, 1.0, 5000, 0.5),
+    disableDepthTestDistance: 1000 // 在1000米内禁用深度测试
+})
 onMounted(() => {
     colors.value = {
         type: 'infinate',
