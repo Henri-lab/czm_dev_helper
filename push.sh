ddf212982157635650e982a32a6386b435818e6f
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-09-14 09:12:10 +0800

    ''

--- push.sh
+++ push.sh
@@ -9,12 +9,12 @@ BRANCH="master"
 BRANCH2='master'
 
 #gitee repo
-until git push -u "$REMOTE2" "$BRANCH2";
-do
-    echo "Gitee push failed, retrying in 1 second..."
-    sleep 1
-done
-echo "Gitee push succeeded."
+# until git push -u "$REMOTE2" "$BRANCH2";
+# do
+#     echo "Gitee push failed, retrying in 1 second..."
+#     sleep 1
+# done
+# echo "Gitee push succeeded."
 
 #github repo 主要是GitHub推送网络问题很严重
 # 循环直到git push成功
