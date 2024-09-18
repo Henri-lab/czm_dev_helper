bc92d304bc5b8d70853048440aa2f2945b3a4261
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-09-18 09:01:32 +0800

    three

---
+++ src/cesium_dev_helper/components/Third/Three.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="three@henrifox" ref="__three__"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+const __three__ =ref();
+onMounted(()=>{
+    // 获取Three.js的Canvas
+const threeCanvas = __three__.value;
+
+// 创建Three.js渲染器
+const threeRenderer = new THREE.WebGLRenderer({
+    canvas: threeCanvas,
+    alpha: true,
+    premultipliedAlpha: false
+});
+threeRenderer.setPixelRatio(window.devicePixelRatio);
+threeRenderer.setSize(window.innerWidth, window.innerHeight);
+threeRenderer.autoClear = false; // 让Cesium先渲染，然后再渲染Three.js内容
+
+// 创建Three.js场景
+const threeScene = new THREE.Scene();
+
+// 创建Three.js相机（将同步Cesium的相机）
+const threeCamera = new THREE.PerspectiveCamera();
+
+// 调整Three.js Canvas的样式以覆盖Cesium
+threeCanvas.style.position = 'absolute';
+threeCanvas.style.top = '0';
+threeCanvas.style.left = '0';
+threeCanvas.style.width = '100%';
+threeCanvas.style.height = '100%';
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/cesium_dev_helper/example/ExThree/index.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div class="ex1" style="display: flex;">
+        <div class="code" ref="code"
+            style="color:antiquewhite; background-color: rgb(25, 27, 22); width: 50%; height: 20%; overflow: scroll; font-size: 16px;">
+        </div>
+
+        <CzmMap width="800px" height="1000px">
+            <CzmCtx></CzmCtx>
+        </CzmMap>
+    </div>
+</template>
+
+<script setup>
+import { marked } from 'marked'
+import codeString from './code.js'
+import { CzmCtx, CzmMap } from '../../components'
+
+const code = ref(null)
+
+const md = marked(`
+\`\`\` js
+${codeString}
+\`\`\`
+`)
+onMounted(() => {
+    code.value.innerHTML = md
+})
+
+</script>
+
+<style lang="scss" scoped></style>
